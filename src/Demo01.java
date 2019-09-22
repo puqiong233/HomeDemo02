07445ce24954ac08ccf520c38bdc799c3011144e
Puqiong233 <puqiong@qq.com>
2019-09-22 22:17:34 +0800

    Maven

---
+++ src/Demo01.java
@@ -0,0 +1,14 @@
+/**
+ * @author PuQiong
+ * @date = 2019/9/22 - 9:50
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        String s = "123";
+        System.out.println(s);
+    }
+
+    public void Nomain() {
+
+    }
+}
